6ff4d67ae32365afc2b04acadc1734f5b1d1f96f
TyurninaMilana <milanatyurnina@gmail.com>
2021-12-20 12:28:01 +0300

    Scripted auto-commit on change (2021-12-20 12:28:01) by gitwatch.sh

--- src/main/java/lab6/Zoo.java
+++ src/main/java/lab6/Zoo.java
@@ -1,4 +1,15 @@
 package lab6;
 
+import akka.actor.ActorRef;
+import org.apache.zookeeper.ZooKeeper;
+
+import java.io.IOException;
+
 public class Zoo {
+    private ZooKeeper zooKeeper;
+    private ActorRef storeActor;
+
+    public Zoo(ActorRef storeActor) throws IOException {
+        this.zooKeeper = new ZooKeeper();
+    }
 }
