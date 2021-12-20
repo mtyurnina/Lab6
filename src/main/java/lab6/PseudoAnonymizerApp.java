f725dd6928175b8c22bbf69562cb41534f6c3165
TyurninaMilana <milanatyurnina@gmail.com>
2021-12-20 11:52:06 +0300

    Scripted auto-commit on change (2021-12-20 11:52:06) by gitwatch.sh

--- src/main/java/lab6/PseudoAnonymizerApp.java
+++ src/main/java/lab6/PseudoAnonymizerApp.java
@@ -1,5 +1,16 @@
 package lab6;
 
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+
+import java.io.IOException;
+
 public class PseudoAnonymizerApp {
 
+    public static void main(String[] args) throws IOException {
+        System.out.println("start!");
+        ActorSystem actorSystem = ActorSystem.create("routes");
+        ActorRef storeActor = actorSystem.actorOf(StoreActor.props(), "storeActor");
+        
+    }
 }
