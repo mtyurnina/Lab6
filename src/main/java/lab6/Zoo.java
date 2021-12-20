7b5e5bcf7f5a8c560ea10cc63e126ae49b3ea888
TyurninaMilana <milanatyurnina@gmail.com>
2021-12-20 12:33:54 +0300

    Scripted auto-commit on change (2021-12-20 12:33:54) by gitwatch.sh

--- src/main/java/lab6/Zoo.java
+++ src/main/java/lab6/Zoo.java
@@ -35,7 +35,7 @@ public class Zoo {
 
     private void serverWatch() {
         try {
-            List<String> serverChildrenNames
+            List<String> serverChildrenNames = 
         }
     }
 }
