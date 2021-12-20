4068875c99010da9a67e7aa2aae0f235835aebdd
TyurninaMilana <milanatyurnina@gmail.com>
2021-12-20 12:00:21 +0300

    Scripted auto-commit on change (2021-12-20 12:00:21) by gitwatch.sh

--- src/main/java/lab6/PseudoAnonymizerApp.java
+++ src/main/java/lab6/PseudoAnonymizerApp.java
@@ -1,16 +1,31 @@
 package lab6;
 
+import akka.NotUsed;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.stream.ActorMaterializer;
+import akka.stream.Server;
 
 import java.io.IOException;
 
 public class PseudoAnonymizerApp {
+
+    private static final String HOST = "localhost";
+    private static final int PORT = 8080;
 
     public static void main(String[] args) throws IOException {
         System.out.println("start!");
         ActorSystem actorSystem = ActorSystem.create("routes");
         ActorRef storeActor = actorSystem.actorOf(StoreActor.props(), "storeActor");
 
+        final Http http = Http.get(actorSystem);
+        final ActorMaterializer actorMaterializer = ActorMaterializer.create(actorSystem);
+
+        Server server = new Server(http, PORT, storeActor);
+
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = server.createRoute().flow(actorSystem, actorMaterializer)
     }
 }
