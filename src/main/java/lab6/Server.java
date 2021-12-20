a523b285e16c35bb839cf1395334c626feee6f03
TyurninaMilana <milanatyurnina@gmail.com>
2021-12-20 12:49:08 +0300

    Scripted auto-commit on change (2021-12-20 12:49:08) by gitwatch.sh

--- src/main/java/lab6/Server.java
+++ src/main/java/lab6/Server.java
@@ -38,7 +38,7 @@ public class Server {
 
     private CompletionStage<HttpResponse> redirect(String url, int count) {
         return Patterns.ask(storeActor, new RandomServerMessage(), duration)
-                .thenCompose(serverURL -> fetch(create))
+                .thenCompose(serverURL -> fetch(createURL))
     }
 
 }
