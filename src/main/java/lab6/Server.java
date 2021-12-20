053cd6a9195c36bd9a9b0466da907804d720de85
TyurninaMilana <milanatyurnina@gmail.com>
2021-12-20 12:41:09 +0300

    Scripted auto-commit on change (2021-12-20 12:41:09) by gitwatch.sh

--- src/main/java/lab6/Server.java
+++ src/main/java/lab6/Server.java
@@ -1,5 +1,15 @@
 package lab6;
 
+import akka.actor.ActorRef;
+import akka.http.javadsl.Http;
+
+import java.time.Duration;
+
 public class Server {
 
+    private Http http;
+    private ActorRef storeActor;
+    private Duration duration = Duration.ofSeconds(5);
+
+    private void zooKeeperIni
 }
