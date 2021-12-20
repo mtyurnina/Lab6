aec5a8dd589e161f5df80409f047d977d0c06863
TyurninaMilana <milanatyurnina@gmail.com>
2021-12-20 12:33:45 +0300

    Scripted auto-commit on change (2021-12-20 12:33:45) by gitwatch.sh

--- src/main/java/lab6/Zoo.java
+++ src/main/java/lab6/Zoo.java
@@ -1,9 +1,14 @@
 package lab6;
 
 import akka.actor.ActorRef;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooDefs;
 import org.apache.zookeeper.ZooKeeper;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 public class Zoo {
 
@@ -14,6 +19,23 @@ public class Zoo {
     private ActorRef storeActor;
 
     public Zoo(ActorRef storeActor) throws IOException {
-        this.zooKeeper = new ZooKeeper(CONNECT, TIMEOUT, );
+        this.zooKeeper = new ZooKeeper(CONNECT, TIMEOUT, null);
+        this.storeActor = storeActor;
+        serverWatch();
+    }
+
+    public void createServer(String serverURL) throws KeeperException, InterruptedException {
+        zooKeeper.create(
+                "/servers/s",
+                serverURL.getBytes(),
+                ZooDefs.Ids.OPEN_ACL_UNSAFE,
+                CreateMode.EPHEMERAL_SEQUENTIAL
+        );
+    }
+
+    private void serverWatch() {
+        try {
+            List<String> serverChildren
+        }
     }
 }
