bd3d4651b25bfa294f652dfa4186e8b39b88c87b
TyurninaMilana <milanatyurnina@gmail.com>
2021-12-20 12:14:10 +0300

    Scripted auto-commit on change (2021-12-20 12:14:10) by gitwatch.sh

--- src/main/java/lab6/StoreActor.java
+++ src/main/java/lab6/StoreActor.java
@@ -2,8 +2,14 @@ package lab6;
 
 import akka.actor.Props;
 
+import java.util.Random;
+
 public class StoreActor {
     public static Props props() {
         return Props.create(StoreActor.class);
+    }
+
+    private String getRandomServer() {
+        return serverList[new Random().nextInt()]
     }
 }
