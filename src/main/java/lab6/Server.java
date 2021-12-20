476d6b9e214c780572ecfc391b48a6281766f87c
TyurninaMilana <milanatyurnina@gmail.com>
2021-12-20 12:44:28 +0300

    Scripted auto-commit on change (2021-12-20 12:44:28) by gitwatch.sh

--- src/main/java/lab6/Server.java
+++ src/main/java/lab6/Server.java
@@ -2,7 +2,9 @@ package lab6;
 
 import akka.actor.ActorRef;
 import akka.http.javadsl.Http;
+import org.apache.zookeeper.KeeperException;
 
+import java.io.IOException;
 import java.time.Duration;
 
 public class Server {
@@ -11,12 +13,12 @@ public class Server {
     private ActorRef storeActor;
     private Duration duration = Duration.ofSeconds(5);
 
-    private void zooKeeperInitialization(int port) throws {
+    private void zooKeeperInitialization(int port) throws IOException, InterruptedException, KeeperException {
         Zoo zoo = new Zoo(storeActor);
         zoo.createServer(getServerURL(port));
     }
 
-    public Server(final Http http, int port, ActorRef storeActor) {
-
+    public Server(final Http http, int port, ActorRef storeActor) throws IOException, InterruptedException, KeeperException {
+        return "http://localhost:"
     }
 }
